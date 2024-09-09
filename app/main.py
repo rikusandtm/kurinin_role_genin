681dc9346bef7e105b5f982d2d57f42da7119533
rikusandtm <rikusan.dtm@gmail.com>
2024-09-09 16:28:34 +0900

    Add files via upload

---
+++ app/main.py
@@ -0,0 +1,29 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user.name} has connected to Discord!')
+
+@bot.event
+async def on_message(message):
+    # 監視するチャンネルのID
+    target_channel_id = 1121209665808781384  # ここにチャンネルのIDを入力
+
+    # メッセージが監視対象のチャンネルで送信された場合
+    if message.channel.id == target_channel_id and len(message.content) >= 5:
+        # メンバーに「下忍」ロールを付与
+        member = message.author
+        guild = message.guild
+        role = discord.utils.get(guild.roles, name='下忍')
+        await member.add_roles(role)
+
+    await bot.process_commands(message)
+
+# Botのトークンを設定
+bot.run('MTE3OTM0ODYwMDM1MDg1NTIyOA.G6jd6h.IG47yVRAHDObaT11DmYfwBHcMx6cKCtPWZEeSA')
